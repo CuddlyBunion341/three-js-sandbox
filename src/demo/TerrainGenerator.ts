23acdaf04594e470a56bf95a417d127944cc652c
Daniel Bengl <daniel.bengl@renuo.ch>
2023-12-25 15:48:19 +0100

    Implement basic terrain generation with perlin

--- src/demo/TerrainGenerator.ts
+++ src/demo/TerrainGenerator.ts
@@ -1,21 +1,32 @@
-import { ChunkData } from "./ChunkData"
+import { NoiseFunction2D, createNoise2D } from "simplex-noise"
+import alea from "alea"
 
 export class TerrainGenerator {
   private seed: number
+  private noise: NoiseFunction2D
   // private noise: any
 
   constructor(seed = 0) {
     this.seed = seed
+
+    const prng = alea(seed)
+    const noise = createNoise2D(prng)
+    this.noise = noise
   }
 
   public getBlock(x: number, y: number, z: number) {
-    const frequency = 0.1
+    // const frequency = 1 / ChunkData.WIDTH * 2
+    // const amplitude = 5
+
+    // const xOffset = Math.sin(x * frequency) * amplitude
+    // const zOffset = Math.sin(z * frequency) * amplitude
+
+    // const surfaceY = 10 + xOffset + zOffset
+
+    const frequency = 0.01
     const amplitude = 5
-
-    const xOffset = Math.sin(x * frequency) * amplitude
-    const zOffset = Math.sin(z * frequency) * amplitude
-
-    const surfaceY = 10 + xOffset + zOffset
+    const minSurfaceY = 10
+    const surfaceY = minSurfaceY + this.noise(x * frequency, z * frequency) * amplitude
 
     return (y < surfaceY) ? 1 : 0
   }
