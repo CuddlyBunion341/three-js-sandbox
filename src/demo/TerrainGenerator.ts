0442c2c408d47ee1c52a256f1057af3256bd94b6
Daniel Bengl <daniel.bengl@renuo.ch>
2023-12-25 15:58:11 +0100

    Refactor TerrainGenerator with LayeredNoise class

--- src/demo/TerrainGenerator.ts
+++ src/demo/TerrainGenerator.ts
@@ -1,32 +1,36 @@
 import { NoiseFunction2D, createNoise2D } from "simplex-noise"
 import alea from "alea"
 
-export class TerrainGenerator {
-  private seed: number
+class LayeredNoise {
+  private baseFrequency = 0.01
+  private baseAmplitude = 5
+  private octaves: number
+
   private noise: NoiseFunction2D
-  // private noise: any
 
-  constructor(seed = 0) {
-    this.seed = seed
-
+  constructor(seed = 0, octaves = 1) {
     const prng = alea(seed)
     const noise = createNoise2D(prng)
+    this.octaves = octaves
     this.noise = noise
   }
 
+  sample2d(x: number, z: number) {
+    const value = this.noise(x * this.baseFrequency, z * this.baseFrequency) * this.baseAmplitude
+    return value
+  }
+}
+
+export class TerrainGenerator {
+  private noise: LayeredNoise
+
+  constructor(seed = 0) {
+    this.noise = new LayeredNoise(seed)
+  }
+
   public getBlock(x: number, y: number, z: number) {
-    // const frequency = 1 / ChunkData.WIDTH * 2
-    // const amplitude = 5
-
-    // const xOffset = Math.sin(x * frequency) * amplitude
-    // const zOffset = Math.sin(z * frequency) * amplitude
-
-    // const surfaceY = 10 + xOffset + zOffset
-
-    const frequency = 0.01
-    const amplitude = 5
     const minSurfaceY = 10
-    const surfaceY = minSurfaceY + this.noise(x * frequency, z * frequency) * amplitude
+    const surfaceY = minSurfaceY + this.noise.sample2d(x, z)
 
     return (y < surfaceY) ? 1 : 0
   }
